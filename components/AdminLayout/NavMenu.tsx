481ba43cbcfd778462459dc6c244ace06a4987b5
CodeDroid99 <johnmwega2002@gmail.com>
2024-02-07 04:17:58 +0300

    Natty

--- components/AdminLayout/NavMenu.tsx
+++ components/AdminLayout/NavMenu.tsx
@@ -7,32 +7,32 @@ const NavigationMenu: React.FC = () => {
       <ul className="navbar-nav p-1 bg-blue-100">
         <li className="nav-item active p-2 ">
           <Link href="/" passHref>
-            <span className="nav-link  pl-3 text-white fint-bold">Home <span className="sr-only">(current)</span></span>
+            <span className="nav-link  pl-3 pt-2 pr-2 pb-2 mb-2 text-white fint-bold">Home <span className="sr-only">(current)</span></span>
           </Link>
         </li>
-        <li className="nav-item p-2 shadow border-2 border-blue-950">
+        <li className="nav-item shadow bg-gray-300 ">
           <Link href="/how-it-works" passHref>
-            <span className="nav-link  pl-3">How it works</span>
+            <span className="nav-link  pl-3 pt-2 pr-2 pb-2 mb-2">How it works</span>
           </Link>
         </li>
-        <li className="nav-item p-2 shadow border-2 border-blue-950">
+        <li className="nav-item shadow bg-gray-300 ">
           <Link href="/login" passHref>
             <span className="nav-link pl-3">Sign in</span>
           </Link>
         </li>
-        <li className="nav-item p-2 shadow border-2 border-blue-950">
+        <li className="nav-item shadow bg-gray-300 ">
           <Link href="/signup" passHref>
             <span className="nav-link pl-3">Sign up +</span>
           </Link>
         </li>
-        <li className="nav-item p-2 shadow border-2 border-blue-950">
+        <li className="nav-item shadow bg-gray-300 ">
           <Link href="/blog" passHref>
             <span className="nav-link pl-3">Blog</span>
           </Link>
         </li>
-        <li className="nav-item p-2 shadow border-2 border-blue-950">
+        <li className="nav-item shadow bg-gray-300 ">
           <Link href="/contact-us" passHref>
-            <span className="nav-link  pl-3">Contact us</span>
+            <span className="nav-link  pl-3 pt-2 pr-2 pb-2 mb-2">Contact us</span>
           </Link>
         </li>
       </ul>
--- components/unAuthed/NavMenu.tsx
+++ components/unAuthed/NavMenu.tsx
@@ -9,38 +9,56 @@ const NavigationMenu: React.FC = () => {
       <ul className="navbar-nav p-1 bg-blue-100">
         <li className="nav-item active p-2 ">
           <Link href="/" passHref>
-            <span className="nav-link  pl-3 text-blue-950 font-bold ">Home <span className="sr-only">(current)</span></span>
+            <span className="nav-link  pl-3  pr-2  text-blue-950 font-bold ">Home <span className="sr-only">(current)</span></span>
           </Link>
         </li>
-        <li className="nav-item p-2 shadow border-2 border-blue-950">
+
+        <li className="nav-item shadow bg-gray-300 ">
           {!user ? (
             <Link href="/signup" passHref>
-              <span className="nav-link  pl-3">Sign Up</span>
+              <span className="nav-link  pl-3 pt-2 pr-2 pb-2 mb-2">Sign Up</span>
             </Link>
           ) : (
             <Link href="/login" passHref>
-              <span className="nav-link  pl-3">Sign In</span>
+              <span className="nav-link  pl-3 pt-2 pr-2 pb-2 mb-2">Sign In</span>
             </Link>
           )}
         </li>
-        <li className="nav-item p-2 shadow border-2 border-blue-950">
-          <Link href="/login" passHref>
-            <span className="nav-link pl-3">Sign in</span>
+        <li className="nav-item shadow bg-gray-300 ">
+          <Link href="/add-booking" passHref>
+            <span className="nav-link pl-3">Book a ticket</span>
           </Link>
         </li>
-        <li className="nav-item p-2 shadow border-2 border-blue-950">
-          <Link href="/signup" passHref>
-            <span className="nav-link pl-3">Sign up +</span>
+        <li className="nav-item shadow bg-gray-300 ">
+          <Link href="/add-hire" passHref>
+            <span className="nav-link pl-3">Hire a bus +</span>
           </Link>
         </li>
-        <li className="nav-item p-2 shadow border-2 border-blue-950">
-          <Link href="/blog" passHref>
-            <span className="nav-link pl-3">Blog</span>
-          </Link>
+        <li className="nav-item shadow bg-gray-300 ">
+          {!user ? (
+            <Link href="/signup" passHref>
+              <span className="nav-link  pl-3 pt-2 pr-2 pb-2 mb-2">Sign Up</span>
+            </Link>
+          ) : (
+            <Link href="/login" passHref>
+              <span className="nav-link  pl-3 pt-2 pr-2 pb-2 mb-2">Sign In</span>
+            </Link>
+          )}
         </li>
-        <li className="nav-item p-2 shadow border-2 border-blue-950">
+        <li className="nav-item shadow bg-gray-300 ">
+          {!user ? (
+            <Link href="/signup" passHref>
+              <span className="nav-link  pl-3 pt-2 pr-2 pb-2 mb-2">About Us</span>
+            </Link>
+          ) : (
+            <Link href="/login" passHref>
+              <span className="nav-link  pl-3 pt-2 pr-2 pb-2 mb-2">Sign In</span>
+            </Link>
+          )}
+        </li>
+        <li className="nav-item shadow bg-gray-300 ">
           <Link href="/contact-us" passHref>
-            <span className="nav-link  pl-3">Contact us</span>
+            <span className="nav-link  pl-3 pt-2 pr-2 pb-2 mb-2">Contact us</span>
           </Link>
         </li>
       </ul>
